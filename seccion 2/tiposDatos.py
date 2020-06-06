1ca0b4e80c529e25f0d1339d1c82856c97900e2c
Enrique Soto <esz22@hotmail.com>
2020-06-06 17:29:55 -0500

    tipos de datos

---
+++ seccion 2/tiposDatos.py
@@ -0,0 +1,10 @@
+x=5
+y=10.1
+z=True
+print(x)
+print(y)
+print(z)
+#La funcion type no se puede mandar a llamar directamente en VSCode
+type(x)
+a="Holi"
+print(a)
---
+++ seccion 2/tiposDatosNumericos.py
@@ -0,0 +1,6 @@
+x=10
+print(x)
+y=10.30
+print(y)
+z=None
+print(z)
