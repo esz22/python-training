46966f52b7cec51a20663ef4db3bfef66b5d488c
Enrique Soto <esz22@hotmail.com>
2020-06-10 22:54:11 -0500

    ejemplo de listas o arreglos

--- seccion 6/listas.py
+++ seccion 6/listas.py
@@ -1,2 +1,11 @@
 nombres=["juan", "carla", "ricardo", "maria"]
 print (nombres)
+print(len(nombres))
+print(nombres[0])
+print(nombres[1])
+print(nombres[2])
+print(nombres[3])
+print(nombres[-1])
+print(nombres[-2])
+print(nombres[-3])
+print(nombres[-4])
