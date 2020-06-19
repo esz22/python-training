e0ba87325258f310d0642004591c581e53b007d3
Enrique Soto <esz22@hotmail.com>
2020-06-18 20:54:25 -0500

    metodos privados en seccion 9 de encapsulamiento

--- seccion 9/metodos_privados.py
+++ seccion 9/metodos_privados.py
@@ -3,4 +3,21 @@ class persona:
         self.nombre=nombre
         self._paterno=ape_paterno
         self.__materno=ape_materno
+    def __metodo_privado(self):
+        print(self.nombre)
+        print(self._paterno)
+        print(self.__materno)
+    def metodo_publico(self):
+        self.__metodo_privado()
+    def get_materno(self):
+        return self.__materno
+    def set_materno(self,apellido):
+        self.__materno=apellido
     
+        
+p1=persona("juan","lopez","perez")
+p1.metodo_publico()
+
+print(p1.nombre)
+print(p1._paterno)
+print(p1.get_materno())
